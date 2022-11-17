4e13ff100076ff5745cb3796c4e842cab7390f4f
vi-u <118408434+vi-u@users.noreply.github.com>
2022-11-17 12:50:31 +1000

    Add files via upload

---
+++ cc_decrypt.py
@@ -0,0 +1,32 @@
+import string
+from time import sleep
+
+alphabet = string.ascii_lowercase # "abcdefghijklmnopqrstuvwxyz"
+
+def decrypt():
+    
+    print("Welcome to Caesar Cipher Decryption.\n")
+    encrypted_message = input("Enter the message you would like to decrypt: ").strip()
+    print()
+    key = int(input("Enter key to decrypt: "))
+    
+    decrypted_message = ""
+
+    for c in encrypted_message:
+
+        if c in alphabet:
+            position = alphabet.find(c)
+            new_position = (position - key) % 26
+            new_character = alphabet[new_position]
+            decrypted_message += new_character
+        else:
+            decrypted_message += c
+
+    print("\nDecrypting your message...\n")
+    sleep(2) # give an appearance of doing something complicated
+    print("Stand by, almost finished...\n")
+    sleep(2) # more of the same
+    print("Your decrypted message is:\n")
+    print(decrypted_message)
+
+decrypt()
